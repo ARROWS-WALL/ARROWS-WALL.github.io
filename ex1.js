49738f3c06336e20a827841446abffc663069050
ARROWS-WALL <shamblo511@gmail.com>
2018-11-27 11:26:46 +0900

    js_css追加

---
+++ ex1.js
@@ -0,0 +1,26 @@
+function message(){
+  alert("こんにちは");
+}
+
+var number=0;
+
+function return0(){
+  number=0;
+  alert(number);
+}
+function add1(){
+  number=number+1;
+  alert(number);
+}
+function subtract1(){
+  number=number-1;
+  alert(number);
+}
+function double(){
+  number=number*2;
+  alert(number);
+}
+function half(){
+  number=number/2;
+  alert(number);
+}
